ea0069a8361efbc39487d677c60239244c95a7cf
Mitch Gilbert <mitchellpgilbert@gmail.com>
2021-01-15 12:00:32 -0800

    complete

--- main.js
+++ main.js
@@ -3,9 +3,37 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+addEventListeners()
+let hidden = document.querySelector('#modal')
+hidden.style.display = 'none'
+//fetch fake server 
+function addEventListeners() {
+  likeButtons = document.querySelectorAll('.like-glyph')
+  likeButtons.forEach(like => like.addEventListener('click', ServerCall))
+}
 
+function ServerCall(e) {
+  mimicServerCall()
+  .then(heart => {
+    heart = e.target
+    if (heart.classList[0] === "activate_heart") {
+      heart.innerHTML = EMPTY_HEART 
+    }
+    else {
+      heart.innerHTML = FULL_HEART
+      heart.classList = "activate_heart"
+    }
+  })
+  .catch(() => {
+    hidden.style.display = 'block'
+    setTimeout(ifError, 5000)
+  })
+}
 
-
+function ifError () {
+  alert("Baby come back")
+  hidden.style.display = 'none'
+}
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
